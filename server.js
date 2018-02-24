d58fae8763716e4b7145ac2908841b1e03d80f9f
Simran Bhamrah <36455112+simi28@users.noreply.github.com>
2018-02-24 17:37:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    var articleName = req.parans.articleName;
+    var articleName = req.params.articleName;
   res.send(createTemp(articles[articleName]));
 });
 
