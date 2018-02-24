b5c156d79ab754ac05b90463e1fb642572cae0a9
Simran Bhamrah <36455112+simi28@users.noreply.github.com>
2018-02-24 22:35:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,13 +16,13 @@ button.onclick=function(){
 };
 
 var nameInput=document.getElementById('name');
-var name=nameInput.value;
+var name1=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
   var names=['name1','name2','name3','name4'];
   var list='';
-  for(var i=0;i<names.length;i++){
-  list+='<li>'+names[i]+'</li>';
+  for(var i=0; i<names.length; i++){
+  list += '<li>'+names[i]+'</li>';
 }
 var ul=document.getElementById('namelist');
 ul.innerHTML=list;
