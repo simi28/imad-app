c74f25f7af7eae1190df7b8401faeabf754b7ef0
Simran Bhamrah <36455112+simi28@users.noreply.github.com>
2018-02-24 20:48:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,9 +13,8 @@ img.onclick=function(){
     var interval=setInterval(moveRight,50);
 };
 
-var counter=0;
-
 var button=document.getElementById('counter');
+var counter=0;
 button.onclick=function(){
   counter=counter+1;
   var span=document.getElementById('count');
