01707fe103e2291d9a0dd12285af7a5414d4bf0d
Simran Bhamrah <36455112+simi28@users.noreply.github.com>
2018-02-24 22:36:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ button.onclick=function(){
 };
 
 var nameInput=document.getElementById('name');
-var name1=nameInput.value;
+var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
   var names=['name1','name2','name3','name4'];
