f5ce0ddb8cdf1e683feb4ade5c5daed673d31883
Simran Bhamrah <36455112+simi28@users.noreply.github.com>
2018-02-24 20:49:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,3 @@
-console.log('Loaded!');
-
-var element=document.getElementById('main-text');
-element.innerHTML='New Value';
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
-};
-
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
