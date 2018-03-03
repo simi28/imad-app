06fe360babbd6292f9013876961d41fd58939c5c
Simran Bhamrah <36455112+simi28@users.noreply.github.com>
2018-03-03 19:56:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
 
+var config={
+    user:'simranbhamrah1997',
+    database:'simranbhamrah1997',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -89,6 +97,20 @@ function createTemp(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool=new Pool(config);
+app.get('/test-db', function(req,res)
+{
+    pool.query('SELECT * from test',function(err,result)
+    {
+       if(err) {
+           res.status(500).send(err.toString());
+       } 
+       else {
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 
 var counter=0;
