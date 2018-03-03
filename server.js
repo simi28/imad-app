b53322143b4d2853d417e4028ef7f90379fc6d6f
Simran Bhamrah <36455112+simi28@users.noreply.github.com>
2018-03-03 20:32:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get('articles/:articleName', function (req, res) {
         } else {
             if(result.rows.length === 0) {
                 res.status(404).send('Article not found');
-            } esle {
+            } else {
                 var articleData=result.rows[0];
                 res.send(createTemplate(articleData));
             }
