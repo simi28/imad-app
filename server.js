02973e426b5ffb2448bfd6ac5d67ece1e9c2793b
Simran Bhamrah <36455112+simi28@users.noreply.github.com>
2018-03-08 16:28:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,24 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function hash(input,salt) {
+    var hashed = crypto.pbkdf2Sync(inout,salt,10000,512,'sha512');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+}
+
+app.get('/create-user',function(req,res) {
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString = hash(password,salt);
+   pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)', [username,dbString], function(err,result) {
+       if(err) {
+           res.status(500).send(err.toString());
+       } 
+       else {
+           res.send(JSON.stringify(result.rows));
+       }
+   });
+});
+
 var pool=new Pool(config);
 app.get('/test-db', function(req,res)
 {
